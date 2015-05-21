1345982232c88857ce72d2426554b334cc5642eb
trisdan <tsleyson@ucdavis.edu>
2015-05-21 00:30:53 -0700

    Moved most of entities.clj to events.clj.

--- madoka_population/src/madoka_population/events.clj
+++ madoka_population/src/madoka_population/events.clj
@@ -2,6 +2,10 @@
   (:require [incanter.stats :as stats]
             [madoka-population.entities :as entities]))
 
+(def ^:dynamic random-source
+  "The source of random numbers."
+  (java.util.Random.))
+
 ;; This is one ass-ugly function. Maybe a square macro would help.
 ;; Note: see http://stackoverflow.com/a/8367547/3376926.
 ;; We don't have a |R0 - R1| <= ... part because that excludes circles
@@ -29,4 +33,64 @@
 
 (defn spawn-magical-girls
   "Each Incubator attempts to spawn a magical girl."
-  )
+  [incubators world-size]
+  (let [successes
+        (->> (repeatedly (count incubators) #(.nextDouble random-source 0 1))
+             (filter #(<= %2 (:success-rate %1)) incubators))]
+    (repeatedly (count successes) (entities/new-magical-girl world-size))))
+
+(defn get-combat-info
+  "Calculates various quantities used for determining outcome of a
+  battle."
+  [combatant1 combatant2]
+  (let [combatants (sort-by :combat
+                            [combatant1 combatant2])]
+    {:weaker (first combatants)
+     :stronger (second combatants)
+     :combat-diff (- (:combat combatant1) (:combat combatant2))}))
+
+;; Note: For now we assume the weaker opponent is the one who
+;; wants to escape, but nothing about this function requires that.
+;; Also, nothing about this function cares whether the opponent
+;; is a witch, a familiar, or another magical girl.
+(defn attempt-escape
+  "If one of the combatants is a magical girl and the difference in
+  strength with her opponent is not too great, she may attempt to
+  escape battle.
+
+  Returns true for a successful escape, false otherwise."
+  [flier combat-diff]
+  (and (can-flee flier combat-diff)
+       (< (rand) (/ (Math/abs combat-diff)
+                    (* 2 (:combat flier))))))
+
+(defn get-victory-interval
+  "Returns interval for a victory for the stronger party."
+  [combat-ratio]
+  (if (> combat-ratio (- 1 combat-ratio))
+    ;; Then [0, combat-ratio] is a larger interval.
+    #(<= 0 % combat-ratio)
+    #(<= 0 % (- 1 combat-ratio))))
+
+(defn determine-outcome
+  [stronger weaker]
+  (let [ratio (/ (:combat weaker) (:combat stronger))
+        in-victory-interval? (get-victory-interval ratio)]
+    (if (in-victory-interval? (rand))
+      stronger
+      weaker)))
+
+(defn fight
+  "Models combat between two entities. Returns the winner."
+  [combatant1 combatant2]
+  {:pre [(not-any? nil? (map :combat [combatant1 combatant2]))]}
+  
+  (let [info (get-combat-info combatant1 combatant2)
+        fled? (attempt-escape (:weaker info) (:combat-diff info))]
+    (cond
+     fled?
+       :fled 
+     (zero? (:combat-diff info))
+       (rand-nth [combatant1 combatant2])
+     :else
+       (determine-outcome (:stronger info) (:weaker info)))))
